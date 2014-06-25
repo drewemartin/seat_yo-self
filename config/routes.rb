38bef83270bacc7dfa2a14cc06d32b659e6a4627
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-06-25 16:41:15 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root "restaurants#index"
+
+  resources :users, :only => [:create, :new]
+
+  resources :reservations
+
+  resources :restaurants, :except => [:destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
